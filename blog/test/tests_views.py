2114b059f37f7f0e2a4087fba4b9c7072bc3cecb
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-03-11 19:53:51 +0000

    created custom 404 error page

--- blog/test/tests_views.py
+++ blog/test/tests_views.py
@@ -1,30 +1,14 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from django.contrib.auth.models import User
-from django.utils.text import slugify
 from blog.models import Post, Comment
 
 
 class TestViews(TestCase):
 
     def setUp(self):
-        client = Client()
-        home_url = reverse('home')
-        blog_url = reverse('blog')
-        
-        self.test_user = User.objects.create(
-            username="test_user",
-            password="password",
-            email="test@test.com"
-        )
-        self.test_post = Post.objects.create(
-            title="Test Title",
-            author=self.test_user,
-            slug=slugify("Test Title"),
-            post_image="a.jpg",
-            content="content"
-        )
-        # self.post_detail_url = reverse('post_detail', kwargs={'slug': self.test_post.slug})
+        self.client = Client()
+        self.home_url = reverse('home')
+        self.blog_url = reverse('blog')
 
     def test_home_GET(self):
 
@@ -38,10 +22,3 @@ class TestViews(TestCase):
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog.html')
-
-    def test_PostDetail_GET(self):
-        response = self.client.get(reverse('post_detail', kwargs={'slug': self.test_post.slug}))
-        print(f"RES: {response}")
-
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_detail.html')
--- blog/urls.py
+++ blog/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('blog/', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
+
+handler404 = "meta_human.views.page_not_found_view"
