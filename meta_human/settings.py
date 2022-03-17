59a95e1151a9e1f4deff846972f970f0df563270
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-03-17 04:51:16 +0000

    added staticfiles to installed apps

--- meta_human/settings.py
+++ meta_human/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
